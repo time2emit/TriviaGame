0bd80ca6cf9dc1545d195b388dfb34e222297afa
Alexis Frank <alexisfrank@alexisfrank-macbookpro2.roam.corp.google.com>
2017-05-24 16:42:55 -0700

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,4 @@
-// timeToComplete
-
-var timeToComplete = 120;
+var timeToComplete = 5;
 
 //Upon click, remove start button, start timer, and show questions
 $('#start').on('click', presentAnswers);
@@ -8,11 +6,13 @@ $('#start').on('click', presentQuestions);
 $('#start').on('click', startTimer);
 $('#start').on('click', removeButton);
 
+
+
 // Show answers function - append answers to a divs
 function presentAnswers(){
 	for (var s = 0; s < californiaAnswers.stateBird.length; s++){
 		$('#a1').append('<input type="radio" class="stateAnswers" name="group1" value="californiaAnswers.stateBird[s]">' + californiaAnswers.stateBird[s]);
-		$('#a2').append('<input type="radio" class="stateAnswers" name="group2" + value="californiaAnswers.stateAmphibian[s]">' + californiaAnswers.stateAmphibian[s]);
+		$('#a2').append('<input type="radio" class="stateAnswers" name="group2" value="californiaAnswers.stateAmphibian[s]">' + californiaAnswers.stateAmphibian[s]);
 		$('#a3').append('<input type="radio" class="stateAnswers" name="group3" value="californiaAnswers.stateAnimal[s]">' + californiaAnswers.stateAnimal[s]);
 		$('#a4').append('<input type="radio" class="stateAnswers" name="group4" value="californiaAnswers.stateFruit[s]">' + californiaAnswers.stateFruit[s]);
 		$('#a5').append('<input type="radio" class="stateAnswers" name="group5" value="californiaAnswers.stateVegetable[s]">' + californiaAnswers.stateVegetable[s]);
@@ -27,19 +27,44 @@ function presentQuestions(){
 	}
 }
 
+//Sets an interval to call the decrementTimer function every second
 function startTimer(){
-	$('#time').text('Countdown: ' + timeToComplete);
+	intervalId = setInterval(decrementTimer, 1000);
+	$('#time').html('Countdown: ' + timeToComplete);
+
 }
 
+//Reduces time by one seconds and fills it into the time div
+
 function decrementTimer(){
+	timeToComplete--;
+	$('#time').html('Countdown: ' + timeToComplete);
+	if (timeToComplete === 0){
+		console.log('end');
+		$('#time').remove();
+		$('#submitButton').remove();
+		$('#formQuestions').remove();
+		$('#questions').html('<div>results</div>');
+	}
+
 
 }
 
 // Removes the button element and adds the submit button
 function removeButton(){
 	$('#start').remove();
-	$('#submit').html('<button id="submitB>Submit</button');
+	$('#submit').html('<button id="submitButton">Submit</button');
 } 
+
+function showAnswers(){
+	if (timeToComplete === 0){
+		console.log('end');
+		$('#time').remove();
+		$('#submitButton').remove();
+		$('#formQuestions').remove();
+		$('#questions').html('<div>results</div>');
+	}
+}
 
 // Questions array and Answers array variables
 
@@ -57,4 +82,3 @@ var californiaAnswers = {
 	stateVegetable: ['sweet onion', 'sugar beet', 'artichoke', 'sweet potato', 'chile'],
 
 };
-
