37414454892dbb67da28fd7472841951e017af2d
Alexis Frank <alexisfrank@alexisfrank-macbookpro2.roam.corp.google.com>
2017-05-29 09:18:08 -0700

    com

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -113,7 +113,7 @@ function showAnswers(){
 		$('#formQuestions').remove();
 		$('#questions').html('<div id="results">Results</div>');
 		$('#questions').html("<div>" + 'Right Answers: ' + rightAnswers + "</div>");
-		$('#questions').append("<div>" + 'Wrong Answers or Empty Answers: ' + wrongAnswers + "</div>");
+		$('#questions').append("<div>" + 'Wrong or Empty Answers: ' + wrongAnswers + "</div>");
 
 	}
 
